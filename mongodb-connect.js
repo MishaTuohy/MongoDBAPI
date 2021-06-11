25cc15cd3d2d42d5688188996b020fbd35a7c8b3
Misha <67553114+MishaTuohy@users.noreply.github.com>
2021-06-11 10:15:52 +0100

    Add files via upload

---
+++ mongodb-connect.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const mongoPath = 'mongodb+srv://cs230:cs230@cluster.f0xng.mongodb.net/CS230?retryWrites=true&w=majority';
+
+
+module.exports = async () => {
+    await mongoose.connect(mongoPath, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    return mongoose;
+}
