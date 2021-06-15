b84f593df8e1dc7755fd81aef23b7745a86d5b20
Misha <67553114+MishaTuohy@users.noreply.github.com>
2021-06-15 11:19:41 +0100

    Update mongodb-connect.js

--- mongodb-connect.js
+++ mongodb-connect.js
@@ -1,7 +1,6 @@
 const mongoose = require("mongoose");
 const mongoPath = 'mongodb+srv://cs230:cs230@cluster.f0xng.mongodb.net/CS230?retryWrites=true&w=majority';
 
-
 module.exports = async () => {
     await mongoose.connect(mongoPath, {
         useNewUrlParser: true,
